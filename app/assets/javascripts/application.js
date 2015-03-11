caf88cf926b6100bd5e4756e5bf9838fff579862
Allison Marino <amarino_us@yahoo.com>
2015-03-11 13:01:33 -0700

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,35 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require skrollr
+//= require_tree .
+
+// ( function( $ ) {
+//     // Init Skrollr
+//     var s = skrollr.init({
+//         render: function(data) {
+//             //Debugging - Log the current scroll position.
+//             //console.log(data.curTop);
+//         }
+//     });
+// } )( jQuery );
+
+function(skrollr){
+    var s = skrollr.init();
+});
+
+// $(document).on('page:load', function () {
+// skrollr.init().refresh(); 
+// });4
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root to: "site#index"
+
+  resources :sites
+
+end
+
+
